46e154ce9c6cb776d0ad03c9ecf577d6cfe38bf9
cycumonk <cycumonk@gmail.com>
2024-06-17 23:08:16 +0800

    create new index

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestfulApi.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Any logic you want to perform when the page is accessed
+        }
+    }
+}
